33996c2fbd97e0ff3e4cd3260402dac507a2ea0d
hhorsekim <hhorsekim@gmail.com>
2022-03-10 08:58:47 +0900

    체크박스_취소선

--- todo/src/List/Data.js
+++ todo/src/List/Data.js
@@ -1,10 +1,19 @@
 function Data() {
-  const parsedDate = new Date().toLocaleDateString('ko-kr');
+  const parsedDate = new Date().toLocaleDateString('ko-kr', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const parsedDay = new Date().toLocaleDateString('ko-kr', {
+    weekday: 'long'
+  })
   
   return (
     <span
       className = "data"
-    >{parsedDate}</span>
+    >{parsedDate} {parsedDay}</span>
+    
   )
 }
 
